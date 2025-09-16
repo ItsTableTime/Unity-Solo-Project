2a7d1722faca8d740d4b195713c5a2f141dbd358
ItsTableTime <166195657+ItsTableTime@users.noreply.github.com>
2025-09-16 10:09:36 -0700

    Jumping

--- Unity Solo Project/Assets/Scripts/PlayerController.cs
+++ Unity Solo Project/Assets/Scripts/PlayerController.cs
@@ -13,6 +13,8 @@ public class PlayerController : MonoBehaviour
     InputAction LookVector;
     Camera PlayerCamera;
 
+    RaycastHit hit;
+    Ray ray;
     Rigidbody rb;
     float verticalMove;
     float horizontalMove;
@@ -21,12 +23,14 @@ public class PlayerController : MonoBehaviour
     public float YSensitivity = 1.0f;
     public int Health = 3;
     public int MaxHealth = 3;
+    public float JumpDistance = 10f;
     public float speed = 15f;
-    public float jumpHeight = 10f;
+    public float jumpHeight = 10000f;
     public float CameraRotationLimit = 90.0f;
 
     public void Start()
     {
+        ray = new Ray(transform.position, transform.forward);
         CameraOffset = new Vector3(0, .3f, .35f);
         rb = GetComponent<Rigidbody>();
         PlayerCamera = Camera.main;
@@ -40,15 +44,12 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
-        if (Health <= 0)
-        {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-        }
-
         // Movement System
         Vector3 temp = rb.linearVelocity;
         temp.x = verticalMove * speed;
         temp.z = horizontalMove * speed;
+        ray.origin = transform.position;
+        ray.direction = -transform.up;
         rb.linearVelocity = (temp.x * transform.forward) + (temp.y * transform.up) + (temp.z * transform.right);
 
         // Camera System
@@ -60,8 +61,11 @@ public class PlayerController : MonoBehaviour
 
         PlayerCamera.transform.rotation = Quaternion.Euler(-CameraRotation.y, CameraRotation.x, 0);
         transform.localRotation = Quaternion.AngleAxis(CameraRotation.x, Vector3.up);
-       
 
+        if(Health <= 0)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
     }
 
     public void Move(InputAction.CallbackContext context)
@@ -72,8 +76,16 @@ public class PlayerController : MonoBehaviour
         horizontalMove = InputAxis.x;
 
     }
+    public void Jump()
+    {
+        if (Physics.Raycast(ray,JumpDistance))
+        {
+            rb.AddForce(transform.up * jumpHeight);
 
-    private void OnTriggerEnter(Collider other)
+
+        }
+    }
+        private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "DeathZone")
         {
@@ -87,5 +99,4 @@ public class PlayerController : MonoBehaviour
             Health -= 1;
         }
     }
-
 }
