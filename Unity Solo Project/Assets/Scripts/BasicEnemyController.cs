67090412a4561963b249099c45e92931749c2b18
ItsTableTime <166195657+ItsTableTime@users.noreply.github.com>
2025-09-18 10:00:42 -0700

    Enemies

--- Unity Solo Project/Assets/Scripts/BasicEnemyController.cs
+++ Unity Solo Project/Assets/Scripts/BasicEnemyController.cs
@@ -1,9 +1,19 @@
+using System.Security.Cryptography;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class BasicEnemyController : MonoBehaviour
 {
     NavMeshAgent agent;
+    public float EnemyVision = 30f;
+    public float EnemyAggro = 30f;
+    public int WanderChance = 60;
+    int IsWander;
+    Vector3 EnemyPosition;
+    Vector3 PlayerPosition;
+    float EnemySight;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -13,6 +23,22 @@ public class BasicEnemyController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        agent.destination = GameObject.Find("Player").transform.position;
+        EnemyPosition = transform.position;
+        PlayerPosition = GameObject.Find("Player").transform.position;
+        EnemySight = Vector3.Distance(EnemyPosition, PlayerPosition);
+        if (EnemySight < EnemyVision)
+        {
+            agent.destination = GameObject.Find("Player").transform.position;
+            EnemyVision = EnemyAggro;
+        }
+        else
+        {
+            IsWander = (RandomNumberGenerator.GetInt32(1, WanderChance));
+            if (IsWander == 1)
+            {
+                agent.destination += new Vector3(RandomNumberGenerator.GetInt32(-9, 9), 0, RandomNumberGenerator.GetInt32(-9, 9));
+            }
+        }
+
     }
 }
